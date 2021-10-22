f69315099446d1d3c52146baaa45352b91b6a54e
dinhhungGM <dinhhungGM@gmail.com>
2021-10-22 16:00:15 +0700

    Change host swagger 2

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['server.js'];
+const endpointsFiles = ['index.js'];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
